36379111c5aafb860589f91fa1640b236b11a5b7
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-09-03 14:20:40 -0300

    Dois tipos de exibição de item

--- app/src/main/java/br/com/adalbertofjr/sunshine/ForecastAdapter.java
+++ app/src/main/java/br/com/adalbertofjr/sunshine/ForecastAdapter.java
@@ -26,18 +26,38 @@ import br.com.adalbertofjr.sunshine.util.Utility;
  */
 public class ForecastAdapter extends CursorAdapter {
 
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return (position == 0) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
     /*
         Remember that these views are reused as needed.
-     */
+         */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
 
-        return view;
+        if (viewType == VIEW_TYPE_TODAY) {
+            layoutId = R.layout.list_item_forecast_today;
+        } else {
+            layoutId = R.layout.list_item_forecast;
+        }
+
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
